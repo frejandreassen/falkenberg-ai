2f56d32c0a398021c3d1ba70695583a1301a603c
Frej Andreassen <frej.andreassen@gmail.com>
2023-05-16 07:28:29 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown('Streamlit is **_really_ cool**.')
+st.markdown('This text is :red[colored red], and this is **:blue[colored]** and bold.')
+st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+file = open('./test.md')
+markdown = file.read()
+st.markdown(markdown)
